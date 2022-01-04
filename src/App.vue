f6ae5d7278d784da7e6ce0f727cf750326f3c519
Petr Ptacek <petr.ptacek99@gmail.com>
2022-01-04 21:29:43 +0100

    Add sass and init sass structure

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,14 @@
 <template>
-  <TheHeader />
-  <img alt="Vue logo" src="@/assets/images/logo.png" />
-  <div class="container">
-    <main>
-      <router-view></router-view>
-    </main>
-  </div>
-  <TheFooter />
+  <AppHeader />
+  <AppContent />
+  <AppFooter />
 </template>
 
 
 <script setup lang="ts">
-import TheHeader from '@/components/TheHeader.vue';
-import TheFooter from '@/components/TheFooter.vue';
-
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import {
+  AppHeader,
+  AppFooter,
+  AppContent
+} from '@/components';
 </script>
-
-
-<style>
-@import "@/styles/global.css";
-</style>
---
+++ src/components/layout/AppContent.vue
@@ -0,0 +1,7 @@
+<template>
+  <div id="content">
+    <main id="main">
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
